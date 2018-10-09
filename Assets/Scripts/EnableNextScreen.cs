5d47df66bc2e5f6dccd991642dad81af457f266e
Halim Lais <halimlais@hotmail.com>
2018-10-09 15:57:34 +0100

    Initial commit

---
+++ Assets/Scripts/EnableNextScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EnableNextScreen : MonoBehaviour {
+
+    [SerializeField] GameObject canvas;
+    AudioSource soundClip;
+    [SerializeField] AudioClip sample;
+
+	// Use this for initialization
+	void Start () {
+
+        soundClip = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    public void TransistionLevel()
+    {
+        canvas.gameObject.SetActive(true);
+    }
+    public void PlayAudio()
+    {
+        soundClip.PlayOneShot(sample);
+    }
+    
+}
